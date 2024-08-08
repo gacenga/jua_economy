57d95c85830a2e66399668791862eb0070de936a
Gacenga <jimwillymurigu@gmail.com>
2024-08-08 22:50:27 +0300

    jua_economy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,36 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager, current_user
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+db = SQLAlchemy()
+csrf = CSRFProtect()
+login = LoginManager()
+migrate = Migrate()
 
-from app import routes
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'jimwilly12345'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///microblog.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    csrf.init_app(app)
+    login.init_app(app)
+    login.login_view = 'login'
+    migrate.init_app(app, db)
+
+    @login.user_loader
+    def load_user(user_id):
+        # Import the User model from the module where it's defined
+        from .models import User
+        return User.query.get(int(user_id))
+
+    with app.app_context():
+        from . import models
+        from .routes import init_routes
+        init_routes(app)
+        db.create_all()
+
+    return app
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,38 @@
-from app import app
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
+from .forms import RegistrationForm, LoginForm
+from .models import User
+from . import db
+from flask_login import login_user, current_user, logout_user
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+def init_routes(app):
+    @app.route('/')
+    def home():
+        return render_template('home.html')
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        return render_template('signup.html', title='Register', form=form)
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                return redirect(url_for('home'))
+            else:
+                flash('Login Unsuccessful. Please check email and password', 'danger')
+        return render_template('login.html', title='Login', form=form)
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return redirect(url_for('home'))
